a26cb81ba038a89befe6d834f765e74d92640d59
FaerieFrOSt <faerie@mint>
2015-01-24 15:41:30 +0100

    Added resource manager

---
+++ include/resourceholder.h
@@ -0,0 +1,25 @@
+
+#ifndef _RESOURCEHOLDER_H_
+# define _RESOURCEHOLDER_H_
+
+#include <map>
+#include <memory>
+#include <string>
+
+template <typename Resource, typename Identifier>
+class	ResourceHolder
+{
+	public:
+		void			load(Identifier id, const std::string &filename);
+		template <typename Parameter>
+		void			load(Identifier id, const std::string &filename, const Parameter& secondParam);
+		Resource&		get(Identifier id);
+		const Resource&	get(Identifier id) const;
+
+	private:
+		std::map<Identifier, std::unique_ptr<Resource>>	mResourceMap;
+};
+
+#include "resourceholder.inl"
+
+#endif /* !_RESOURCEHOLDER_H_ */
