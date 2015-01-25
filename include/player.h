8b706471943228df48d390abd274e4e10bfb7836
FaerieFrOSt <faerie@mint>
2015-01-25 15:13:05 +0100

    Finished personalisable keys

--- include/player.h
+++ include/player.h
@@ -8,8 +8,30 @@
 class	Player
 {
 	public:
+		enum	Action
+		{
+			MoveLeft,
+			MoveRight,
+			MoveUp,
+			MoveDown,
+			Debug,
+		};
+
+		Player();
 		void	handleEvent(const sf::Event& event, CommandQueue& commands);
 		void	handleRealtimeInput(CommandQueue& commands);
+
+		void				assignKey(Action action, sf::Keyboard::Key key);
+		sf::Keyboard::Key	getAssignedKey(Action action) const;
+
+		bool	isRealtimeAction(Action action) const;
+
+	private:
+		static bool	isRealTimeAction(Action action);
+
+	private:
+		std::map<sf::Keyboard::Key, Action>	mKeyBinding;
+		std::map<Action, Command>	mActionBinding;
 };
 
 #endif /* !_PLAYER_H_ */
--- src/player.cpp
+++ src/player.cpp
@@ -2,50 +2,75 @@
 #include "aircraft.h"
 #include <iostream>
 
+Player::Player()
+{
+	mKeyBinding[sf::Keyboard::Left] = MoveLeft;
+	mKeyBinding[sf::Keyboard::Right] = MoveRight;
+	mKeyBinding[sf::Keyboard::Up] = MoveUp;
+	mKeyBinding[sf::Keyboard::Down] = MoveDown;
+	mKeyBinding[sf::Keyboard::P] = Debug;
+
+	const float	playerSpeed = 40.f;
+	mActionBinding[MoveLeft].action = derivedAction<Aircraft>(AircraftMover(-playerSpeed, 0.f));
+	mActionBinding[MoveRight].action = derivedAction<Aircraft>(AircraftMover(playerSpeed, 0.f));
+	mActionBinding[MoveUp].action = derivedAction<Aircraft>(AircraftMover(0.f, -playerSpeed));
+	mActionBinding[MoveDown].action = derivedAction<Aircraft>(AircraftMover(0.f, playerSpeed));
+	mActionBinding[Debug].action = [] (SceneNode& node, sf::Time)
+	{
+		std::cout << node.getWorldPosition().x << ", " << node.getWorldPosition().y << std::endl;
+	};
+
+	for (auto& pair : mActionBinding)
+		pair.second.category = Category::PlayerAircraft;
+}
+
 void	Player::handleEvent(const sf::Event& event, CommandQueue& commands)
 {
-	if (event.type == sf::Event::KeyPressed && event.key.code == sf::Keyboard::P)
-	{
-		Command	output;
-		output.category = Category::PlayerAircraft;
-		output.action = [] (SceneNode& s, sf::Time)
-		{
-			std::cout << s.getWorldPosition().x << ", " << s.getWorldPosition().y << std::endl;
-		};
-		commands.push(output);
-	}
+	if (event.type != sf::Event::KeyPressed)
+		return;
+	for (auto pair : mKeyBinding)
+		if (event.key.code == pair.first && !isRealtimeAction(pair.second))
+			commands.push(mActionBinding[pair.second]);
 }
 
 void	Player::handleRealtimeInput(CommandQueue& commands)
 {
-	const float	playerSpeed = 40.f;
+	for (auto pair : mKeyBinding)
+		if (sf::Keyboard::isKeyPressed(pair.first) && isRealtimeAction(pair.second))
+			commands.push(mActionBinding[pair.second]);
+}
 
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Up))
+void	Player::assignKey(Action action, sf::Keyboard::Key key)
+{
+	for (auto it = mKeyBinding.begin(); it != mKeyBinding.end();)
 	{
-		Command	move;
-		move.category = Category::PlayerAircraft;
-		move.action = derivedAction<Aircraft>(AircraftMover(0.f, -playerSpeed));
-		commands.push(move);
+		if (it->second == action)
+			mKeyBinding.erase(it++);
+		else
+			++it;
 	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Down))
+	mKeyBinding[key] = action;
+}
+
+sf::Keyboard::Key	Player::getAssignedKey(Action action) const
+{
+	for (auto it = mKeyBinding.begin(); it != mKeyBinding.end(); ++it)
+		if (it->second == action)
+			return it->first;
+	return mKeyBinding.end()->first;
+}
+
+bool	Player::isRealtimeAction(Action action) const
+{
+	switch (action)
 	{
-		Command	move;
-		move.category = Category::PlayerAircraft;
-		move.action = derivedAction<Aircraft>(AircraftMover(0.f, playerSpeed));
-		commands.push(move);
-	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Left))
-	{
-		Command	move;
-		move.category = Category::PlayerAircraft;
-		move.action = derivedAction<Aircraft>(AircraftMover(-playerSpeed, 0.f));
-		commands.push(move);
-	}
-	if (sf::Keyboard::isKeyPressed(sf::Keyboard::Right))
-	{
-		Command	move;
-		move.category = Category::PlayerAircraft;
-		move.action = derivedAction<Aircraft>(AircraftMover(playerSpeed, 0.f));
-		commands.push(move);
+		case MoveUp:
+		case MoveDown:
+		case MoveLeft:
+		case MoveRight:
+			return true;
+		default:
+			return false;
 	}
 }
+
